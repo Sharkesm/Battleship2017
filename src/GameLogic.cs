1419a85b3011d6d28b261c475b0d5ef5a95c44b8
hasano14 <bang_apeez@yahoo.com>
2017-04-03 23:52:45 +0800

    Add files via upload

---
+++ src/GameLogic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using (Microsoft.VisualBasic;)
+{
+using (System;)
+{
+using (System.Collections;)
+{
+using (System.Collections.Generic;)
+{
+using (System.Data;)
+{
+using (System.Diagnostics;)
+{
+public static class GameLogic { static @void Main() {; //Opens a new Graphics Window
+		SwinGame.OpenGraphicsWindow("Battle Ships", 800, 600);
+
+		//Load Resources
+		GameResources.LoadResources();
+
+		SwinGame.PlayMusic(GameResources.GameMusic("Background"));
+
+		//Game Loop
+		do { GameController.HandleUserInput();
+			GameController.DrawScreen(); } while (!(SwinGame.WindowCloseRequested() == true | GameController.GameController.CurrentState == GameState.Quitting));
+
+		SwinGame.StopMusic();
+
+		//Free Resources and Close Audio, to end the program.FreeResources(); } }
+
+//======================================================= //Service provided by Telerik(www.telerik.com)
+//Conversion powered by NRefactory.//Twitter
+telerik;
+//Facebook
+facebook.com / telerik;
+//======================================================= //=======================================================
