ad05bb7f384a48442a048943fa6398b1d6bf4ebc
sharkesm <sharkesm@gmail.com>
2017-04-06 01:53:04 +0800

    Fix file with c# syntax

--- src/GameLogic.cs
+++ src/GameLogic.cs
@@ -1,35 +1,25 @@
-﻿using System;
-using (Microsoft.VisualBasic;)
+﻿internal static class GameLogic
 {
-using (System;)
-{
-using (System.Collections;)
-{
-using (System.Collections.Generic;)
-{
-using (System.Data;)
-{
-using (System.Diagnostics;)
-{
-public static class GameLogic { static @void Main() {; //Opens a new Graphics Window
+	public static void Main()
+	{
+		//Opens a new Graphics Window
 		SwinGame.OpenGraphicsWindow("Battle Ships", 800, 600);
 
 		//Load Resources
-		GameResources.LoadResources();
+		LoadResources();
 
-		SwinGame.PlayMusic(GameResources.GameMusic("Background"));
+		SwinGame.PlayMusic(GameMusic("Background"));
 
 		//Game Loop
-		do { GameController.HandleUserInput();
-			GameController.DrawScreen(); } while (!(SwinGame.WindowCloseRequested() == true | GameController.GameController.CurrentState == GameState.Quitting));
+		do
+		{
+			HandleUserInput();
+			DrawScreen();
+		} while (!(SwinGame.WindowCloseRequested() == true || CurrentState == GameState.Quitting));
 
 		SwinGame.StopMusic();
 
-		//Free Resources and Close Audio, to end the program.FreeResources(); } }
-
-//======================================================= //Service provided by Telerik(www.telerik.com)
-//Conversion powered by NRefactory.//Twitter
-telerik;
-//Facebook
-facebook.com / telerik;
-//======================================================= //=======================================================
+		//Free Resources and Close Audio, to end the program.
+		FreeResources();
+	}
+}
